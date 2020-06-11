b495c755f6b5e9717e35cdf86efe7e1ded935357
Thomas Monfre <tmonfre1@gmail.com>
2020-06-11 11:46:29 -0500

    update zoom.tar.gz

--- deploy.sh
+++ deploy.sh
@@ -21,14 +21,29 @@ rm -rf bin/zoom.tar.gz
 # move to bin (gitignored and will clear out on build)
 mv zoom.tar.gz bin
 
-# generate sha256
-shasum -a 256 bin/zoom.tar.gz | awk '{printf $1}' | pbcopy
-
 # upload to server
-server upload bin/zoom.tar.gz
+rm -rf ../personal-server/static/zoom.tar.gz
+cp bin/zoom.tar.gz ../personal-server/static/zoom.tar.gz 
+cd ../personal-server/static/zoom.tar.gz
+git add -A 
+git commit -m "update zoom.tar.gz"
+git push
+cd ../zoom
 
 # uninstall via dotnet
 make uninstall
 
+# generate sha256 signature and update in homebrew repository
+shatmp="    sha256 \"$(shasum -a 256 bin/zoom.tar.gz | awk '{printf $1}')\""
+sed -i.bak "5s/.*/$shatmp/" ../homebrew-tmonfre/zoom.rb 
+unset shatmp
+
+# push homebrew repo
+cd ../homebrew-tmonfre
+git add -A
+git commit -m "update sha256"
+git push
+cd ../zoom
+
 # send done message
-echo "Successfully deployed to server. sha256 signature copied to clipboard. Update sha256 field in zoom.rb in tmonfre/homebrew-tmonfre then push to GitHub for homebrew to reflect the changes."
+echo "Successfully uploaded to server and updated homebrew tap"
