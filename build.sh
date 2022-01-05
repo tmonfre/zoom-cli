f148ffea19f855b0a3794c562409ea5c4038333e
Thomas Monfre <tmonfre1@gmail.com>
2022-01-05 13:35:12 -0600

    feat: update build.sh to generate tarball + sha256

--- build.sh
+++ build.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 pyinstaller cli.py --name zoom --onefile
+
+cd ./dist
+tar zcvf zoom.tar.gz zoom
+cd ../
+
+sha256="$(shasum -a 256 ./dist/zoom.tar.gz | awk '{printf $1}')"
+
+echo ""
+echo "#####################################"
+echo "Generated SHA-256:"
+echo $sha256
+echo "#####################################"
+echo ""
